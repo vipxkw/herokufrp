512039ee45fbc596fd484f93d57957555bc7e345
vipxkw <47981717+vipxkw@users.noreply.github.com>
2021-02-12 22:07:38 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,13 +4,7 @@ cd /frps
 
 cat <<-EOF > /frps/frps.ini
 [common]
-bind_port = 80
-dashboard_port = 443
-token = 12345678vipiu
-dashboard_user = admin
-dashboard_pwd = admin
-vhost_http_port = 10080
-vhost_https_port = 10443
+bind_port = $PORT
 EOF
 
 echo $PORT
